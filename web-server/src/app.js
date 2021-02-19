d2985aa80245a8a7e96fbffaff28f25b9de6be51
RobPerez155 <robert.perez.psirho@gmail.com>
2021-02-18 19:32:22 -0500

    Completed up to Sect. 49

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,26 +1,41 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
 
-// app.com
-app.get('', (req, res) => { // This method takes two arguments, the route and a function. 
-//The function gets called with 2 arguments, req(request) and the res(response)
-  res.send('<h1>Hello express!</h1>') // We can send back HTML
-})
-// app.com/help
-app.get('/help', (req, res) => {
-  res.send([{
-    name: 'Robert',
-    age: 32
-  }, {
-    name: 'Kat',
-    age: 35
-  }]) // We can send back 1 JSON object or an array of JSON objects
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public') //here we are creating a directory path
+const viewsPath = path.join(__dirname, '../templates')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs') // Setting up handlebars(dyanmic templating npm)
+app.set('views', viewsPath)// Here we are telling express to use this folder
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+//new hbs index route
+app.get('', (req, res) => {
+  res.render('index', { 
+    title: 'Weather',
+    name: 'Rob Perez'
+  }) 
+  //Our second argument in render will give a value to what we call on in our index.hbs file, in this case the title and name
+  //On #12 we do not need the file extensions, we only need the file name -- res.render converts our hbs file into to html
 })
 
-// app.com/about
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help Page',
+    helpText: 'If you came here looking for help, you\'re in real trouble because there ain\'t none to be found here.'
+  })
+})
+
 app.get('/about', (req, res) => {
-  res.send('<h1>About Page</h1>')
+  res.render('about', {
+    title: 'About Me',
+    name: 'Rob Perez'
+  })
 })
 
 // app.com/weather
