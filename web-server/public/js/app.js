7e2a28522fbdda8ab9b5dbb8bf94551de932c2ea
RobPerez155 <robert.perez.psirho@gmail.com>
2021-02-22 18:11:09 -0500

    Section 9 Completed

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,27 @@
-console.log('Client side javascript file is loaded')
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const message1 = document.querySelector('#message-1')
+const message2 = document.querySelector('#message-2')
+
+weatherForm.addEventListener("submit", (e) => {
+  //e for Event, this is how we'll commonly see this
+  e.preventDefault();
+  message1.textContent = 'Loading';
+  message2.textContent = '';
+
+  const location = search.value; //value will get us the input value
+
+  fetch(`http://localhost:3000/weather?address=${location}`).then(
+    (response) => {
+      response.json().then((data) => {
+        //here we are converting response to a JSON file
+        if (data.error) {
+          message1.textContent = data.error; //textContent will update the value of the selected element
+        } else {
+          message1.textContent = data.forecast;
+          message2.textContent = data.location;
+        }
+      });
+    }
+  );
+});
