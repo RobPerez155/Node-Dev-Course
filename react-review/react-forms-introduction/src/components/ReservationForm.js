aae70623df4489cb0d42660e17a1ce45ec952ace
RobPerez155 <robert.perez.psirho@gmail.com>
2021-03-07 22:13:03 -0500

    Lesson Complete - Build a form - no persist

---
+++ react-review/react-forms-introduction/src/components/ReservationForm.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react"
+
+const ReservationForm = props => {
+  const [firstName, setFirstName] = useState("")
+
+  const handleFirstNameChange = event => {
+    setFirstName(event.currentTarget.value)
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    //send the name up the component hierarchy
+    props.onNameSubmitted(firstName)
+
+    //clear form
+    setFirstName("")
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="first_name">First Name</label>
+        <input id="first_name" type="text" value={firstName} onChange={handleFirstNameChange}/>
+        <input type="submit" value="RSVP" className="btn"/>
+      </div>
+    </form>
+  )
+}
+
+export default ReservationForm
