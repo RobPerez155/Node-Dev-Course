015625103fad85964b102f870b7c89af8eeb51d6
RobPerez155 <robert.perez.psirho@gmail.com>
2021-03-09 15:36:59 -0500

    Advanced React Complete

--- react-review/advanced-react-forms/src/components/App.js
+++ react-review/advanced-react-forms/src/components/App.js
@@ -5,11 +5,16 @@ import MealForm from "./MealForm"
 const App = props => {
   const [meals, setMeals] = useState([])
 
+  const mealSubmittedHandler = meal => {
+    setMeals([...meals, meal])
+  }
+
   return (
     <div className="row">
       <div className="small-9 small-centered columns">
         <h1 className="text-center">Food Tracker</h1>
         <MealsList meals={meals} />
+        <MealForm onMealSubmitted={mealSubmittedHandler} />
       </div>
     </div>
   )
--- react-review/advanced-react-forms/src/components/MealForm.js
+++ react-review/advanced-react-forms/src/components/MealForm.js
@@ -3,40 +3,61 @@ import React, { useState } from "react"
 const meals = ["", "breakfast", "lunch", "dinner", "snack"]
 
 const MealForm = props => {
-  const mealOptions = meals.map(meal => {
-    return(
-      <option key={meal} value={meal}>{meal}</option>
-    )
-  })
-  
   const [mealRecord, setMealRecord] = useState({
     food: "",
     meal: "",
     description: ""
   })
 
+  const mealOptions = meals.map(meal => {
+    return(
+      <option key={meal} value={meal}>{meal}</option>
+    )
+  })
+
+  const handleInputChange = event => {
+    setMealRecord({
+      ...mealRecord, [event.currentTarget.name]: event.currentTarget.value
+    })
+  }
+
+  const clearForm = event => {
+    event.preventDefault()
+    setMealRecord({
+      food: "",
+      meal: "",
+      description: ""
+    })
+  }
+
+  const onSubmitHandler = event => {
+    event.preventDefault()
+    props.onMealSubmitted(mealRecord)
+  }
+
   return (
-    <form className="callout">
+    <form className="callout" onSubmit={onSubmitHandler}>
       <label>
         I ate:
-          <input type="text" name="food" />
+          <input type="text" name="food" onChange={handleInputChange} value={mealRecord.food}/>
       </label>
 
       <label>
         Meal:
-        <select name="meal">{mealOptions}</select>
+        <select name="meal" onChange={handleInputChange} value={mealRecord.meal}>{mealOptions}</select>
       </label>
 
       <label>
         Description:
-        <textarea name="description" />
+        <textarea name="description" onChange={handleInputChange} value={mealRecord.description}/>
       </label>
 
       <div className="button-group">
-        <button className="button">Clear</button>
+        <button className="button" onClick={clearForm}>Clear</button>
         <input className="button" type="submit" value="Submit" />
       </div>
     </form>
+
   )
 }
 
