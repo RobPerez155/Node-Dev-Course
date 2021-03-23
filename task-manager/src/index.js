69d3a3354b23aaa1f3a707a757e2903e95e7b414
RobPerez155 <robert.perez.psirho@gmail.com>
2021-03-22 21:23:49 -0400

    Lesson 99 Completed - Patch Endpoint

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -9,6 +9,8 @@ const port = process.env.PORT || 3000 //part one is what we need to have in orde
 
 app.use(express.json()) // This will automatically parse incoming JSON to an object where we can access it using our req handlers
 
+//------------ Users ------------
+
 app.post('/users', async (req, res) => { // /users is the endpoint
   const user = new User(req.body)
 
@@ -45,6 +47,33 @@ app.get('/users/:id', async (req, res) => {
   }
 })
 
+app.patch('/users/:id', async (req, res) => {
+  // Code needed to send a 400 code for when a user tries to update a property that cannot be change. like _id which can't be updated or height which doesn't exist.
+  const updates = Object.keys(req.body) // Returns an array of strings, where each string is a property of our object
+  const allowedUpdates = ['name', 'email', 'password', 'age'] // Specifies the properties that can be changed
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update)) // this will tell us if every single update can be found in allowed updates - this is written in the arrow function shorthand
+
+    if (!isValidOperation) { // Here we are checking to see if "isValidOperation" is false 
+      return res.status(400).send({ error: 'Invalid update!'})
+    }
+
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true}) // new will return the new user as opposed to the existing one, Validators will make sure we don't take in garbage.
+
+    if (!user) { // If the id cannot be found this will happen 
+      return res.status(404).send()
+    }
+
+    res.send(user) // If a user is found then their info will be returned.
+  } catch (e) {
+    res.status(400).send(e)
+  }
+})
+
+
+
+//------------ Tasks ------------
+
 app.post('/tasks', async (req, res) => {
   const task = new Task(req.body)
   
@@ -77,6 +106,28 @@ app.get('/tasks/:id', async (req, res) => {
   }
     catch(e) {
     res.status(500).send()
+  }
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+  const updates = Object.keys(req.body)
+  const allowedUpdates = ['completed', 'description']
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid task!'})
+  }
+  
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators:true})
+    
+    if(!task) {
+      res.status(404).send()
+    }
+
+    res.send(task)
+  } catch (e) {
+    res.status(400).send(e)
   }
 })
 
