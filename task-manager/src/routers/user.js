5b72298c4404301118ba66a67e8ae67772a33c20
RobPerez155 <robert.perez.psirho@gmail.com>
2021-03-27 12:30:50 -0400

    Lesson 113 - Auth User Endpoints - Completed

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -55,23 +55,7 @@ router.get('/users/me', auth, async (req, res) => {
   res.send(req.user) // Allows a user to get their profile when they're authenticated
 })
 
-router.get('/users/:id', async (req, res) => {
-  const _id = req.params.id
-
-  try {
-    const user = await User.findById(_id)
-
-    if (!user) {
-      return res.status(404).send()
-    }
-
-    res.send(user)
-  } catch (e) {
-    res.status(500).send()
-  }
-})
-
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
   // Code needed to send a 400 code for when a user tries to update a property that cannot be change. like _id which can't be updated or height which doesn't exist.
   const updates = Object.keys(req.body) // Returns an array of strings, where each string is a property of our object
   const allowedUpdates = ['name', 'email', 'password', 'age'] // Specifies the properties that can be changed
@@ -82,32 +66,19 @@ router.patch('/users/:id', async (req, res) => {
     }
 
   try {
-    // const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true}) // new will return the new user as opposed to the existing one, Validators will make sure we don't take in garbage.
-    const user = await User.findById(req.params.id)
-
-    updates.forEach((update) => user[update] = req.body[update]) // This func. will run for each update that is being applied // bracket notation is used around the update variable so it can stay dynamic. // using dot notation would make it static
-
-    await user.save() // this is waiting on our userSchema.pre('save') function in the /models/user file
-
-    if (!user) { // If the id cannot be found this will happen 
-      return res.status(404).send()
-    }
-
-    res.send(user) // If a user is found then their info will be returned.
+    updates.forEach((update) => req.user[update] = req.body[update]) // This func. will run for each update that is being applied // bracket notation is used around the update variable so it can stay dynamic. // using dot notation would make it static
+    await req.user.save() // this is waiting on our userSchema.pre('save') function in the /models/user file
+    
+    res.send(req.user) // If a user is found then their info will be returned.
   } catch (e) {
     res.status(400).send(e)
   }
 })
 
-router.delete('/users/:id', async (req, res) => {
-  try {
-    const user = await User.findByIdAndDelete(req.params.id)
-
-    if (!user) {
-      return res.status(404).send()
-    }
-
-    res.send(user)
+router.delete('/users/me', auth, async (req, res) => {
+  try { //We get the user object from information passed by auth
+    await req.user.remove()
+    res.send(req.user)
   } catch (e) {
     res.status(500).send()
   }
