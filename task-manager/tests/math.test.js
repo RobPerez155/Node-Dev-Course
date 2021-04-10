a64ad24db7bdec937f50550e88d6c01ad90b9667
RobPerez155 <robert.perez.psirho@gmail.com>
2021-04-10 13:01:17 -0400

    Lesson 140 - Testing Async Code - Complete

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,5 +1,5 @@
 // const math = require('../src/math') // This imports the whole file
-const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit, bigButts } = require('../src/math') // This destructures the file to pull in only the functions we need
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit, add } = require('../src/math') // This destructures the file to pull in only the functions we need
 
 // This is the basic way to write a test
 // test('should calculate total with tip', () => {
@@ -29,4 +29,31 @@ test('should convert 0C to 32F', () => {
   expect(celsiusToFahrenheit(0)).toBe(32)
 })
 
+// This test will not throw an error because Jest does not know this test has asynchronous code
+// test('Async test demo', () => {
+//   setTimeout(() => {
+//     expect(1).toBe(2)
+//   }, 2000)
+// })
 
+// This test will throw an error because Jest sees our parameter of done() and it won't consider this test a success or a failure until done() is called. done() can be called anything
+// test('Async test demo', (done) => {
+//   setTimeout(() => {
+//     expect(1).toBe(2)
+//     done()
+//   }, 2000)
+// })
+
+// Both methods of using done() and async/await are valid, but async/await are most common
+
+test('should add two numbers', (done) => {
+  add(2, 3).then((sum) => { //here we call .then() allowing us to provide a function to run when the promise has been fulfilled by a value, in this case the sum 
+    expect(sum).toBe(5)
+    done()
+  })
+})
+
+test('should add two numbers async/await', async () => {
+  const sum = await add(10, 22)
+  expect(sum).toBe(32)
+})
