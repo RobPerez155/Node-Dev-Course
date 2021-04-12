968b9e06c5b51ec27bd82a78eedf728e62df4e05
RobPerez155 <robert.perez.psirho@gmail.com>
2021-04-12 12:20:24 -0400

    Lesson 147 - User Tests extended - Complete

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -23,7 +23,9 @@ beforeEach( async () => { // This will runn before each test case
 
 test('should signup a new user', async () => {
 //await |request(app) from #1 & #2|.post request to create a user|.send({ our data})
-  const response = await request(app).post('/users').send({
+  const response = await request(app)
+  .post('/users')
+  .send({
     name: 'Big Hungry Donkey',
     email: 'microdonk@gmail.com',
     password: 'SmartAss!'
@@ -63,7 +65,7 @@ test('should not login nonexistent user', async () => {
       password: 'none'
   }).expect(400)
 })
-//
+
 test('should get profile for user', async () => {
   await request(app)
     .get('/users/me')
@@ -96,4 +98,41 @@ test('should not delete account for unauthenticated user', async () => {
     .delete('/users/me')
     .send()
     .expect(401)
+})
+
+test('should upload avatar image', async () => {
+  await request(app)
+    .post('/users/me/avatar')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)// Assigns a token to auth the tester
+    .attach('avatar', 'tests/fixtures/profile-pic.jpg') // ('The form field we want to set', 'path to the file')
+    .expect(200)
+
+    // Asserts that binary data for the upload was saved as a buffer
+    const user = await User.findById(userOneId)
+    expect(user.avatar).toEqual(expect.any(Buffer)) // .any is searching for any type of file, in this case Buffer, but can be used w/ String, Number, etc.
+    // Using .toBe is akin ===. eg. 1 === 1 is true but {} === {} is false, because they are just two diff objects with the same properties.
+    // Using .toEqual will compare the obj's properties to confirm if they are the same
+})
+
+test('should update valid user fields', async () => {
+  await request(app)
+    .patch('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      name: 'PinkyPie'
+    })
+    .expect(200)
+
+    // Asserting that the new user.name has been updated
+    const user = await User.findById(userOneId)
+    expect(user.name).toEqual('PinkyPie') // Must use toEqual because we are comparing obj properties
+})
+
+test('should not update invalid user fields', async () => {
+  await request(app)
+    .patch('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      _id: 2
+    }).expect(400)
 })
